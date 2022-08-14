fb23f57408b03a74473f1b75e2cef1ce63675b7e
Programming Hero <programming.hero1@gmail.com>
2022-08-13 22:49:29 -0500

    added some utilities function

--- js/deposit.js
+++ js/deposit.js
@@ -1,7 +1,23 @@
-document.getElementById('btn-deposit').addEventListener('click', function(){
+
+document.getElementById('btn-deposit').addEventListener('click', function () {
     /* 
     1. get the element by id
     2. get the value from the element
     3. convert string value to a number
     */
+    const newDepositAmount = getInputFieldValueById('deposit-field');
+    /* 
+    1. get previous deposit total by id
+    */
+    const previousDepositTotal = getTextElementValueById('deposit-total');
+    
+    // calculate new deposit total
+    const newDepositTotal = previousDepositTotal + newDepositAmount;
+    // set deposit total value
+    setTextElementValueById('deposit-total', newDepositTotal);
+
+    // get previous balance by using the function
+    const previousBalanceTotal = getTextElementValueById('balance-total')
+    const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+    setTextElementValueById('balance-total', newBalanceTotal);
 })
--- js/utilities.js
+++ js/utilities.js
@@ -1,2 +1,19 @@
-const myUtility = 'variable Inside utility file';
-console.log('this is utilities file')
+function getInputFieldValueById(inputFieldId) {
+    const inputField = document.getElementById(inputFieldId);
+    const inputFieldValueString = inputField.value;
+    const inputFieldValue = parseFloat(inputFieldValueString);
+    inputField.value = '';
+    return inputFieldValue;
+}
+
+function getTextElementValueById(elementId) {
+    const textElement = document.getElementById(elementId);
+    const textElementValueString = textElement.innerText;
+    const textElementValue = parseFloat(textElementValueString);
+    return textElementValue;
+}
+
+function setTextElementValueById(elementId, newValue){
+    const textElement = document.getElementById(elementId);
+    textElement.innerText = newValue;
+}
